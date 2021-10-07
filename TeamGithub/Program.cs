1842e0c603c701886394d514d5598e925f123a5b
Josh <thejosh2021@gmailcom>
2021-10-07 13:20:59 -0400

    basic project

---
+++ TeamGithub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeamGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool isNum1Larger(int num1, int num2)
+        {
+            return true;
+        }
+
+        public static bool isNum2Larger(int num1, int num2)
+        {
+            return true;
+        }
+    }
+}
